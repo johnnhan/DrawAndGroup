c8309ba59b9126c48d8a26a1ed6cb51b52df8479
John Han <mrht@qq.com>
2019-08-13 09:35:20 +0800

    a draw and group tool writing in c#

---
+++ Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace DrawAndGroup
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.importBt = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // importBt
+            // 
+            this.importBt.Location = new System.Drawing.Point(13, 13);
+            this.importBt.Name = "importBt";
+            this.importBt.Size = new System.Drawing.Size(75, 24);
+            this.importBt.TabIndex = 0;
+            this.importBt.TabStop = false;
+            this.importBt.Text = "导入TXT";
+            this.importBt.UseVisualStyleBackColor = true;
+            this.importBt.Click += new System.EventHandler(this.importBt_Click);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(116, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "尚未导入任何文件";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // textBox1
+            // 
+            this.textBox1.CausesValidation = false;
+            this.textBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(302, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.textBox1.MaxLength = 4;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(37, 23);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "2";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(271, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 24);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "数量";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 24);
+            this.button1.TabIndex = 6;
+            this.button1.TabStop = false;
+            this.button1.Text = "分组";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(408, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 24);
+            this.button2.TabIndex = 7;
+            this.button2.TabStop = false;
+            this.button2.Text = "一次抽取";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(489, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 24);
+            this.button3.TabIndex = 8;
+            this.button3.TabStop = false;
+            this.button3.Text = "连续抽取";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(94, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseEnter += new System.EventHandler(this.pictureBox1_MouseEnter);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 88);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(257, 272);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(307, 88);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(257, 272);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "待操作数据：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(305, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "操作结果：";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(508, 59);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(56, 24);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "导出";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 5000;
+            this.toolTip1.InitialDelay = 200;
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ReshowDelay = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 372);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.importBt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "分组抽签工具";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button importBt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrawAndGroup
+{
+    public partial class Form1 : Form
+    {
+        private int continueStatus;
+        private string[] tempArr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            continueStatus = 0;
+            tempArr = null;
+        }
+
+        private LinkedList<string> initItemArray()
+        {
+            string[] rep = { "\r\n" };
+            string[] tempArr = textBox2.Text.Split(rep, StringSplitOptions.RemoveEmptyEntries);
+            LinkedListNode<string> prelln = null;
+            LinkedList<string> ll = new LinkedList<string>();
+            for (int i = 0; i < tempArr.Length; i++)
+            {
+                LinkedListNode<string> lln = new LinkedListNode<string>(tempArr[i]);
+                if (i == 0) ll.AddFirst(lln);
+                else if (i == tempArr.Length - 1) ll.AddLast(lln);
+                else ll.AddAfter(prelln, lln);
+                prelln = lln;
+            }
+            return ll;
+        }
+
+        private string[] getItemArray()
+        {
+            LinkedList<string> filterArray = initItemArray();
+            int len = filterArray.Count, cur = 0;
+            string[] arr = new string[len];
+            Random rand = new Random();
+            while (len > 0)
+            {
+                arr[cur] = filterArray.ElementAt(rand.Next(0, len)).ToString();
+                filterArray.Remove(arr[cur]);
+                len = filterArray.Count;
+                cur++;
+            }
+            return arr;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text[0] == '0') textBox1.Text = textBox1.Text.TrimStart('0');
+            if (textBox1.Text == "" || textBox2.Text.Trim() == "") return;
+            continueStatus = 0;
+            string[] newArr = getItemArray();
+            int cou = int.Parse(textBox1.Text);
+            decimal num = Math.Ceiling(decimal.Divide(newArr.Length, cou));
+            string ft = "";
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                if (i % num == 0)
+                {
+                    if (i != 0) ft += "\r\n";
+                    ft = ft + "第" + (i / num + 1).ToString() + "组：\r\n";
+                }
+                ft = ft + newArr[i] + "\r\n";
+            }
+            textBox3.Text = ft;
+        }
+
+        private void importBt_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "ext files (*.txt)|*.txt";
+            DialogResult result = ofd.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string filename = ofd.FileName;
+                string safefilename = ofd.SafeFileName;
+                string text = File.ReadAllText(filename);
+                textBox2.Text = text;
+                label1.Text = ofd.SafeFileName;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox3.Text.Trim() == "") {
+                MessageBox.Show("导出数据不能为空！", "提示");
+                return;
+            }
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            folder.Description = "选择文件保存目录";  //定义在对话框上显示的文本
+            DialogResult result = folder.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string addr = folder.SelectedPath;
+                string fineName = "/draw_and_group_" + GetTimeStamp() + ".txt";
+                File.WriteAllText(addr + fineName, textBox3.Text, Encoding.UTF8);
+            }
+        }
+
+        private static string GetTimeStamp()
+        {
+            TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return Convert.ToInt64(ts.TotalMilliseconds).ToString();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // 阻止从键盘输入键
+            e.Handled = true;
+            if (e.KeyChar >= '0' && e.KeyChar <= '9' || e.KeyChar == '\b') e.Handled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text[0] == '0') textBox1.Text = textBox1.Text.TrimStart('0');
+            if (textBox1.Text == "" || textBox2.Text.Trim() == "") return;
+            continueStatus = 0;
+            string[] newArr = getItemArray();
+            int cou = int.Parse(textBox1.Text);
+            string ft = "";
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                if (i >= cou) break;
+                ft = ft + newArr[i] + "\r\n";
+            }
+            textBox3.Text = ft;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text[0] == '0') textBox1.Text = textBox1.Text.TrimStart('0');
+            if (textBox2.Text.Trim() == "") return;
+            if (continueStatus == 0) {
+                textBox3.Text = "";
+                tempArr = getItemArray();
+            }
+            if (continueStatus >= tempArr.Length) {
+                MessageBox.Show("所有数据已抽取完毕！");
+                return;
+            }
+            textBox3.Text = textBox3.Text + tempArr[continueStatus] + "\r\n";
+            continueStatus++;
+        }
+
+        private void pictureBox1_MouseEnter(object sender, EventArgs e)
+        {
+            string msg = "1.请导入 txt 文件（UTF-8格式），一条数据占一行\n2.数量只作用于分组和一次抽取，连续抽取不受影响";
+            toolTip1.Show(msg, pictureBox1, 0, -68);
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            toolTip1.Hide(pictureBox1);
+        }
+    }
+}
